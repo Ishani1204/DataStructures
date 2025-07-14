25a53e65b2f06bb814a83ceb5a6c35e571ba091b
Ishani1204 <ib622256@gmail.com>
2025-07-15 00:39:40 +0530

    Completed all Bit Manipulation.

--- BitManipulation.java
+++ BitManipulation.java
@@ -31,6 +31,36 @@ public class BitManipulation {
         int bitMask = a|b;
         return n & bitMask;
     }
+
+    public static boolean checkpowerofTwo(int n){
+        if((n&(n-1))==0){
+            return true;
+        }
+        return false;
+    }
+
+    public static int countSetBits(int n){
+        int count = 0;
+        while(n>0){
+            if((n&1) !=0){
+                count++;
+            }
+          n=n>>1;
+        }
+        return count ;
+    }
+
+    public static int fastExpo(int a , int n){
+        int ans =1 ;
+        while(n>0){
+            if((n&1)!=0){
+                ans = ans *a;
+            }
+            a = a*a;
+            n = n>>1;}
+        return ans ;
+    }
+
     public static void main(String[] args){
-    System.out.println(clearIBitsInRange(10 , 2 , 4)); }
+    System.out.println(fastExpo(3 , 5)); }
 }
