8c016bc50ceed00e5485e3f9df41e4e53dd99386
Ishani1204 <ib622256@gmail.com>
2025-07-17 01:17:55 +0530

    Recursion Completed.

--- Recursion.java
+++ Recursion.java
@@ -35,8 +35,70 @@ public static int sum(int n){
     int Sn = n + sum(n-1);
     return Sn;
 }
+//fibonacci numbers
+public static int Fib(int n){
+ if(n==0 || n==1){
+    return n;
+ }
+ int fnm1 = Fib(n-1);
+ int fnm2 = Fib(n-2);
+ int fn = fnm1 + fnm2 ;
+ return fn;
+}
+//sorted array
+public static boolean isSorted(int arr[] , int i){
+    if(i== arr.length-1){
+        return true ;
+    }
+    if (arr[i]>arr[i+1])
+    {return false;}
+
+    return isSorted(arr , i+1);
+}
+//First occurence
+public static int FirstOccurence(int arr[] , int key , int i){
+    if(i==arr.length-1){
+        return -1;
+    }
+    if(arr[i]==key){
+        return i;
+    }
+    return FirstOccurence(arr, key, i+1);
+}//last occurence
+public static int lastOccurence(int arr[] , int key , int i){
+    if(i == arr.length-1){
+        return -1;
+    }
+    int isFound = lastOccurence(arr, key, i+1);
+    if(isFound == -1 && arr[i] == key){
+        return i;
+    }
+    return isFound;
+}
+//power x raised to n
+public static int power(int x , int n){
+   if(n==0){
+    return 1 ;
+   }
+    //int xnm1 = power(x ,n-1);
+    //int xn = x * xnm1;
+    //return xn;
+  return x*power(x , n-1);
+}
+//Optimized code for this power function
+public static int optimizedPower(int a , int n){
+    if(n==0){
+        return 1 ;
+    }
+    int halfPowerSq = optimizedPower(a, n/2) * optimizedPower(a, n/2);
+    //n is odd
+    if(n % 2 != 0){
+        halfPowerSq = a * halfPowerSq;
+    }
+    return halfPowerSq;
+}
+
     public static void main(String args[]){
-        int n = 3;
-        System.out.println(sum(n));
+        System.out.println(optimizedPower(2,4));
     }
 }
