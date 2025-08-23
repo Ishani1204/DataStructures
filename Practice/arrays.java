0dbd08ebf974bdeecfdfc1716674d38037e9d585
Ishani1204 <ib622256@gmail.com>
2025-08-23 17:49:51 +0530

    New algo

--- Practice/arrays.java
+++ Practice/arrays.java
@@ -13,10 +13,19 @@ public class arrays {
         return false;
     }
      // Correct Kadanes Algo
+     // This is correct kadanes algo that works for both set of only negative numbers and both negative and posotive number
+    public static int maxSubArray(int[] nums) {
+        int ms = nums[0];
+        int cs = nums[0] ;
+        for(int i= 1 ; i<nums.length ; i++){
+            int num = nums[i];
+           cs = Math.max(num, cs+num);
+           ms = Math.max(ms , cs);
+    }
+    return ms;}
 
 
-
-     //
+    //Printing subarrays
      public static void subarrays(int num[]){
     for(int i=0;i<num.length;i++){
         for(int j=i;j<num.length;j++){
