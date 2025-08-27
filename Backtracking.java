59e266ba87bc8898df805a8f062ae67dc99f47b4
Ishani1204 <ib622256@gmail.com>
2025-08-27 23:42:39 +0530

    Sudoku code initial commit.

--- Backtracking.java
+++ Backtracking.java
@@ -53,11 +53,30 @@ public class Backtracking {
         findPermuations(Newstr, ans+curr);
     }
    }
+    //Sudoku code
+    public static boolean sudokuSolver(int sudoku[][] , int row , int col){
+        //base case
 
+        //recursion
+        int nextRow = row , nextCol= col+1;
+
+
+        for(int digit=1; digit<=9 ; digit++){
+
+        }
+        return true;
+    }
 
 
     public static void main(String args[]){
-        String str = "abc";
-        findPermuations(str, " ");
+        int sudoku[][] = {{0,0,8,0,0,0,0,0,0} ,
+    {4,9,0,1,5,7,0,0,2} ,
+    {0,0,3,0,0,4,1,9,0} ,
+    {1,8,5,0,6,0,0,2,0} ,
+    {0,0,0,0,2,0,0,6,0} ,
+    {9,6,0,4,0,5,3,0,0} ,
+    {0,3,0,0,7,2,0,0,4} ,
+    {0,4,9,0,3,0,0,5,7} ,
+    {8,2,7,0,0,9,0,1,3}};
     }
 }
