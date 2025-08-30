6572b7ed5d1d4d604b49895cc66e18d4cfc675fb
Ishani1204 <ib622256@gmail.com>
2025-08-30 22:48:56 +0530

    N queens code

--- Backtracking.java
+++ Backtracking.java
@@ -53,6 +53,7 @@ public class Backtracking {
         findPermuations(Newstr, ans+curr);
     }
    }
+   //sudoku part
    //is safe function for sudoku
    public static boolean isSafe(int sudoku[][], int row , int col, int digit){
    //column condition
@@ -82,7 +83,7 @@ public class Backtracking {
   }
   return true;
 }
-//print sudoku
+    //print sudoku
 public static void printSudoku(int sudoku[][]){
     for(int i=0 ; i<9; i++){
         for(int j=0 ; j<9 ;j++){
@@ -121,24 +122,70 @@ public static void printSudoku(int sudoku[][]){
     }
  return false;    
 }
+ //n queens
+    public static void nQueens(char board[][] , int row){
+        // base case
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+        // try placing queen in each column of current row
+        for(int j =0 ; j<board.length ; j++){
+            if(isSafe(board , row , j)){
+                board[row][j] = 'Q'; // place queen
+                nQueens(board, row+1); // recurse to next row
+                board[row][j] = 'x' ; // backtrack
+            }
+        }
+    }
 
+    // check if safe to place queen
+    public static boolean isSafe(char board[][] , int row , int col){
+        // vertical up
+        for(int i=row-1; i>=0;i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+
+        // diagonal left up
+        for(int i=row-1 , j=col-1; i>=0 && j>=0;i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        // diagonal right up
+        for(int i=row-1, j=col+1 ; i>=0 && j<board.length; i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // print board in box format
+    public static void printBoard(char board[][]){
+        System.out.println("Solution:");
+        for(int i =0 ; i<board.length; i++){
+            for(int j=0 ; j<board.length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------");
+    }
 
     public static void main(String args[]){
-    int sudoku[][] = {{0,0,8,0,0,0,0,0,0} ,
-    {4,9,0,1,5,7,0,0,2} ,
-    {0,0,3,0,0,4,1,9,0} ,
-    {1,8,5,0,6,0,0,2,0} ,
-    {0,0,0,0,2,0,0,6,0} ,
-    {9,6,0,4,0,5,3,0,0} ,
-    {0,3,0,0,7,2,0,0,4} ,
-    {0,4,9,0,3,0,0,5,7} ,
-    {8,2,7,0,0,9,0,1,3}};
-    
-  if(sudokuSolver(sudoku , 0 ,0)){
-   System.out.println("Solution exists");
-   printSudoku(sudoku);}
-   else{
-    System.out.println("Soltuion doesn't exists");
-   }  
-  }
+        System.out.println("------Chess Board Solutions------");
+        int n = 4;
+        char board[][] = new char[n][n];
+        // initialize board
+        for(int i=0; i<n ; i++){
+            for(int j=0 ; j<n ; j++){
+                board[i][j] = 'x' ;
+            }
+        }
+        nQueens(board, 0);
+    }
 }
