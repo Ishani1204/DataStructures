501309cc93c2dba9b962b9235d422b4f832fd669
Ishani1204 <ib622256@gmail.com>
2025-09-20 17:10:18 +0530

    Merge Sort and Zig Zag LL

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+//import java.util.LinkedList;
 public class LinkedList {
     //initialisation of the class node
     public static class Node{
@@ -278,6 +278,100 @@ public class LinkedList {
         }
         //remove cycle -> last.next = null
           prev.next = null;
+    }
+
+    public Node mergeSort(Node head){
+        //base case
+        if(head == null || head.next == null){
+            return head;
+        }
+        //find mid
+        Node mid = getMid(head);
+       //left and right half call merge sort
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+       //call merge
+       return merge(newLeft , newRight);
+    }
+
+     private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; //+1 jump
+            fast = fast.next.next; //+2 jump
+        }
+        return slow; 
+    }
+
+    private Node merge(Node head1 , Node head2){
+            Node mergedLL = new Node(-1);
+            Node temp = mergedLL;
+        
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public void zigZag(){
+        //find mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        //reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+       Node left = head;
+       Node right = prev;
+       Node nextL , nextR;
+       
+       //alt merge - zig-zag merge
+       while(left != null && right!= null){
+        nextL = left.next;
+        left.next = right;
+        nextR = right.next;
+        right.next= nextL;
+        
+        left = nextL;
+        right = nextR;
+
+       }
     }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
