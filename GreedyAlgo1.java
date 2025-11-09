087bc0dc2530b5cfe04f46db9fae31e83ef9292c
Ishani1204 <ib622256@gmail.com>
2025-11-09 20:41:19 +0530

    Greedy algo part 2.

--- GreedyAlgo1.java
+++ GreedyAlgo1.java
@@ -34,29 +34,25 @@ public class GreedyAlgo1{
             System.out.print("A"+ans.get(i)+" ");
         }    
         System.out.println();
-
-//CASE 1 : end time basis sorted - O(n)
-public class Activity{
-        int maxAct = 0 ;
-        ArrayList<Integer> ans = new ArrayList<>(); 
-
-        //1st Activity
-        maxAct = 1;
-        ans.add(0);
-        int lastEnd = end[0];
-        for(int i=1; i<end.length; i++){
-            if(start[i] >= lastEnd){
-                //activity select
-                maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
-            }
-        }
-        System.out.println("max activities = " + maxAct);
-        for(int i=0 ; i<ans.size(); i++){
-            System.out.print("A"+ans.get(i)+" ");
-        }    
-        System.out.println();
-        }
+// //CASE 1 : end time basis sorted - O(n)
+//         int maxAct = 0 ;
+//         ArrayList<Integer> ans = new ArrayList<>(); 
+//         //1st Activity
+//         maxAct = 1;
+//         ans.add(0);
+//         int lastEnd = end[0];
+//         for(int i=1; i<end.length; i++){
+//             if(start[i] >= lastEnd){
+//                 //activity select
+//                 maxAct++;
+//                 ans.add(i);
+//                 lastEnd = end[i];
+//             }
+//         }
+//         System.out.println("max activities = " + maxAct);
+//         for(int i=0 ; i<ans.size(); i++){
+//             System.out.print("A"+ans.get(i)+" ");
+//         }    
+//         System.out.println();
     }
 }
---
+++ GreedyAlgo2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class GreedyAlgo2 {
+    public static void main(String args[]){
+        int val[] = {60, 100, 120};
+        int weight[] = {10, 20, 30};
+        int W= 50;
+        //2d array to sort the ratio in descending or ascending order
+        //0th colu => idx; 1st col stores => ratio
+        double ratio[][] = new double[val.length][2];
+        
+        for(int i=0; i<val.length; i++){
+            ratio[i][0] = i;
+            ratio[i][1] = val[i]/(double)weight[i];
+        }
+
+        //ascending order sort 
+        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+       
+        int capacity = W;
+        int finalVal = 0;
+        for(int i= ratio.length-1; i>=0; i--){
+            int idx = (int)ratio[i][0];
+            if(capacity >= weight[idx]){
+                //include full item
+                finalVal += val[idx];
+                capacity -= weight[idx];
+            } else{
+                //include fractional item
+                finalVal += (ratio[i][1] * capacity);
+                capacity = 0;
+                break;
+            }
+        }
+    System.out.print("Final Value " + finalVal);
+    }
+}
