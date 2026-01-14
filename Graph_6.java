0be726d0f9653a4c916cfcf1797531f7d938822f
Ishani1204 <ib622256@gmail.com>
2026-01-14 09:25:52 +0530

    Data Structures.

--- Graph_6.java
+++ Graph_6.java
@@ -117,11 +117,96 @@ public static void tarjanBridge(ArrayList<Edge> graph[], int V){
     }
 
 }
+
+//Articulation point - O(V+E)
+static class edge1 {
+    int src;
+    int dest;
+
+    public edge1(int s, int d){
+        this.src = s;
+        this.dest = d;
+    }
+}
+
+public static void Creategraph(ArrayList<edge1> graph[]){
+    for(int i=0; i<graph.length; i++){
+        graph[i] = new ArrayList<edge1>();
+    }
+
+    graph[0].add(new edge1(0, 1));
+    graph[0].add(new edge1(0, 2));
+    graph[0].add(new edge1(0, 3));
+
+    graph[1].add(new edge1(1, 0));
+    graph[1].add(new edge1(1, 2));
+
+    graph[2].add(new edge1(2, 0));
+    graph[2].add(new edge1(2, 1));
+
+    graph[3].add(new edge1(3, 0));
+    graph[3].add(new edge1(3, 4));
+
+    graph[4].add(new edge1(4, 3));
+    }
+public static void dfs(ArrayList<edge1> graph[], int curr, int par, 
+                        int dt[], int low[], int time, boolean vis[], boolean ap[]){
+    vis[curr] = true;
+    dt[curr] = low[curr] = ++time;
+    int children = 0;
+
+    for(int i=0; i<graph[curr].size(); i++){
+        edge1 e = graph[curr].get(i);
+        int neigh = e.dest;
+
+        if(par == neigh){
+            continue;
+        } else if(vis[neigh]){
+            low[curr] = Math.min(low[curr], dt[neigh]);
+        } else {
+            dfs(graph, neigh, curr, dt, low, time, vis, ap);
+            low[curr] = Math.min(low[curr], low[neigh]);
+            if(par != -1 && dt[curr] <= low[neigh]){
+                //System.out.println("Ap" +curr);
+                ap[curr] = true;
+            }
+            children++;
+        }
+    }
+    if(par == -1 && children > 1){
+        //System.out.println("Ap" +curr);
+        ap[curr] = true;
+    }
+                        }
+public static void getAP(ArrayList<edge1> graph[], int V){
+    int dt[] = new int[V];
+    int low[] = new int[V];
+    int time = 0;
+    boolean vis[] = new boolean[V];
+//Sometimes articulation point value starts repeating itslef in case of large graph so for that we have this
+    boolean ap[] = new boolean[V];
+
+    for(int i=0; i<V; i++){
+        if(!vis[i]){
+         dfs(graph, i, -1, dt, low, time, vis, ap);
+        }
+    }
+
+    //print all APs
+    for(int i=0; i<V; i++){
+        if(ap[i]){
+         System.out.println("AP : " + i);
+        }
+    }
+} 
 public static void main(String args[]){
     int V = 5;
     ArrayList<Edge> graph[] = new ArrayList[V];
     createGraph(graph);
     kosaraju(graph, V);
     tarjanBridge(graph, V);
+
+    ArrayList<edge1> graph1[] = new ArrayList[V];
+    getAP(graph1, V);
     }
 }
